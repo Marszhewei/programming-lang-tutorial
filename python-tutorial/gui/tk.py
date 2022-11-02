05c56ea4e090c2b436930b649f2e57ab94ba5800
fetch150zy <Mars_zhewei@outlook.com>
2022-11-02 18:07:19 +0800

    gui in python

---
+++ python-tutorial/gui/tk.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+        
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text='Hello', command=self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfo('Message', 'Hello, %s' % name)
+
+
+def main():
+    app = Application()
+    app.master.title('hello')
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
